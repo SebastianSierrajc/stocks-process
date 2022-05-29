9680b932e7b44dd91a04ba3d2a2ee36d3745b0af
Sebastian Sierra <sebastian.sierrajc@gmail.com>
2022-05-29 12:33:19 -0500

    script for kafka download and init

---
+++ kafka-queue/kafka-init.sh
@@ -0,0 +1,14 @@
+wget https://dlcdn.apache.org/kafka/3.2.0/kafka_2.13-3.2.0.tgz
+
+tar -xzf kafka_2.13-3.2.0.tgz
+cd kafka_2.13-3.2.0
+
+bin/zookeeper-server-start.sh config/zookeeper.properties &
+bin/kafka-server-start.sh config/server.properties &
+
+ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+port=9092
+topic=stocks-process
+
+bin/kafka-topics.sh --create --topic $topic --bootstrap-server $ip4:$port &
+ 
